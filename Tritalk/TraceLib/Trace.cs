a09c399b0077794a11c80ecb419c18c28a5796b0
MyFunc <de5171245@gmail.com>
2017-01-14 22:43:37 +0200

    Trace proto created

---
+++ Tritalk/TraceLib/Trace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tritalk.Libs
+{
+    [Serializable]
+    public class Trace
+    {
+        public string ID { get; set; }
+        public string Method { get; set; }
+        public string properties { get; set; }
+    }
+}
